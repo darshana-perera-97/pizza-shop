2fb19ac1919046269e6a219c92b1d5df994bb39a
Darshana Perera <darshana.saluka.10@gmail.com>
2022-01-17 20:27:22 +0530

    Site completed

--- src/Design/Design.js
+++ src/Design/Design.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Body from "./Body/Body";
+import Loading from "./Loading/Loading";
 import Topbar from "./TopBar/Topbar";
 
 export default function Design() {
+  const [state, setstate] = React.useState(false);
+  setTimeout(() => {
+    setstate(true);
+  }, 3000);
   return (
     <div>
-      <Topbar />
-      <Body />
+      {!state && <Loading />}
+      {state && <Topbar />}
+      {state && <Body />}
     </div>
   );
 }
--- src/Design/Loading/Loading.js
+++ src/Design/Loading/Loading.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import logo from "./logo.png";
+import "./styles.css";
 
 export default function Loading() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p style={{ margin: "0px", marginTop: "50px", fontSize: "30px" }}>
+            Welcome to
+          </p>
+          <p style={{ margin: "0px", fontSize: "45px", fontFamily: "Lobster" }}>
+            Chilaw Pizza Palace
+          </p>
+        </header>
+      </div>
+    </div>
+  );
 }
